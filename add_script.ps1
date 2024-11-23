b56eb2d58b9a3766b7076953e51b380ccd159551
SplatCreator <37952073+SplatCreator@users.noreply.github.com>
2024-11-23 05:16:48 +0100

    Update add_script.ps1

--- add_script.ps1
+++ add_script.ps1
@@ -3,6 +3,12 @@ param (
     [string]$line_to_add
 )
 
+# Vérification si le répertoire existe
+if (-Not (Test-Path $directory)) {
+    Write-Host "Le répertoire spécifié n'existe pas : $directory"
+    exit
+}
+
 # Boucle à travers chaque fichier HTML, ASPX, et ASPXC
 Get-ChildItem -Recurse -Path $directory -Include *.html, *.aspx, *.aspxc | ForEach-Object {
     $file = $_.FullName
