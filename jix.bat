dca882db2e19b5d37d9424e8052ab10826d12667
SplatCreator <37952073+SplatCreator@users.noreply.github.com>
2024-11-23 05:10:25 +0100

    Update jix.bat

--- jix.bat
+++ jix.bat
@@ -1,23 +1,14 @@
 @echo off
 setlocal enabledelayedexpansion
 
-
+:: Définir le répertoire contenant les fichiers HTML et ASPX
 set "directory=C:\1Energia\Web\ESOIv02\"
-
 
 :: La ligne à ajouter dans le body
 set "line_to_add=<script async src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5908571316079556' crossorigin='anonymous'></script>"
 
-:: Boucle à travers chaque fichier HTML, ASPX et ASPXC
-for /r "%directory%" %%f in (*.html *.aspx *.aspxc) do (
-    powershell -Command "
-        $content = Get-Content '%%f' -Raw
-        if ($content -match '</body>') {
-            $content = $content -replace '</body>', '$line_to_add`$&'
-            Set-Content '%%f' -Value $content
-        }
-    "
-)
+:: Appeler le script PowerShell
+powershell -ExecutionPolicy Bypass -File "C:\1Energia\Web\ESOIv02\papamaman\add_script.ps1" -directory "%directory%" -line_to_add "%line_to_add%"
 
 echo Line added to all HTML, ASPX, and ASPXC files.
 pause
